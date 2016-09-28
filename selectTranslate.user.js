9e0f842d15caea6b41d75a7dfd5923b1f81cfe36
jone <jone.liu@etocrm.com>
2016-09-28 13:13:52 +0800

    修改

--- selectTranslate.user.js
+++ selectTranslate.user.js
@@ -16,32 +16,26 @@ function getSelect() {
 }
 function BSkeyDown(e) {
   if (84 == e.keyCode) {
-    var content = getSelect();
-    var yurl = 'http://fanyi.youdao.com/openapi.do?keyfrom=selectTranslate&key=509196210&type=data&doctype=json&version=1.1&q=' + content;
+    var word = getSelect();
+    var yurl = 'http://fanyi.youdao.com/openapi.do?keyfrom=selectTranslate&key=509196210&type=data&doctype=json&version=1.1&q=' + word;
     GM_xmlhttpRequest({
       method: 'GET',
       url: yurl,
       onload: function(response){
         obj = JSON.parse(response.response);
         content = obj.basic.explains;
-        showTranslate(content);
+        showTranslate(content, word);
       }
     });
   }
 }
 
-function showTranslate(content){
+function showTranslate(content, title){
   var div = document.createElement('div');
-  div.style.position = 'fixed';
-  div.style.top='5px';
-  div.style.right='5px';
-  div.style.borer="2px solid blue";
-  div.style.padding="0 10px 10px";
-  div.style.background="grey";
-  div.style.color="white";
   div.id = '_translate';
+  div.setAttribute('style','position:fixed;top:5px;right:5px;border:2px solid blue;padding:0 10px 10px;background:grey;color:white;z-index:9999;');
   
-  var inner = '<div style="background:blue;color:black;padding:6px 10px;margin:0 -10px;">article</div>\
+  var inner = '<div style="background:blue;color:black;padding:6px 10px;margin:0 -10px;border-bottom:1px solid white;">'+title+'</div>\
   <ol style="padding-left:15px;margin:10px 2px;font-size:15px;">';
   for (var i = content.length - 1; i >= 0; i--) {
     inner += '<li>'+content[i]+'</li>';
@@ -53,6 +47,6 @@ function showTranslate(content){
   
   setTimeout(function(){
     document.getElementById('_translate').remove();
-  },7000);
+  },10000);
 }
 document.onkeydown = BSkeyDown;
